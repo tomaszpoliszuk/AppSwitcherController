1c054a9f7f1f3bf19bcc29cdbc8dd2539f0d4d56
Tomasz Poliszuk <tomaszpoliszuk@users.noreply.github.com>
2020-10-27 21:37:59 +0100

    updated tweak settings code

--- AppSwitcherControllerSettings/AppSwitcherControllerMainSettings.mm
+++ AppSwitcherControllerSettings/AppSwitcherControllerMainSettings.mm
@@ -1,8 +1,25 @@
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
-#include <spawn.h>
 
-NSString *domainString = @"com.tomaszpoliszuk.appswitchercontroller";
+//typedef enum {
+//	None					= 0,
+//	RestartRenderServer		= (1 << 0), // 1 in decimal, also relaunch backboardd
+//	SnapshotTransition		= (1 << 1), // 2 in decimal
+//	FadeToBlackTransition	= (1 << 2), // 4 in decimal
+//} SBSRelaunchActionStyle;
+
+NSString *const domainString = @"com.tomaszpoliszuk.appswitchercontroller";
+
+@interface BSAction : NSObject
+@end
+@interface SBSRelaunchAction : BSAction
++ (id)actionWithReason:(id)arg1 options:(unsigned long long)arg2 targetURL:(id)arg3;
+@end
+
+@interface FBSSystemService : NSObject
++ (id)sharedService;
+- (void)sendActions:(id)arg1 withResult:(id /* block */)arg2;
+@end
 
 @interface PSControlTableCell : PSTableCell
 - (UIControl *)control;
@@ -27,7 +44,6 @@ NSString *domainString = @"com.tomaszpoliszuk.appswitchercontroller";
 	}
 	return _specifiers;
 }
-
 - (instancetype)init {
 	self = [super init];
 	if (self) {
@@ -36,13 +52,13 @@ NSString *domainString = @"com.tomaszpoliszuk.appswitchercontroller";
 	}
 	return self;
 }
-
 -(void)respringDevice {
 	UIAlertController *confirmRespringAlert = [UIAlertController alertControllerWithTitle:@"Respring Device" message:@"Do you want to respring device?" preferredStyle:UIAlertControllerStyleAlert];
 	UIAlertAction *confirm = [UIAlertAction actionWithTitle:@"Confirm" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-		pid_t pid;
-		const char *argv[] = {"sbreload", NULL};
-		posix_spawn(&pid, "/usr/bin/sbreload", NULL, NULL, (char* const*)argv, NULL);
+		SBSRelaunchAction *respringAction = [NSClassFromString(@"SBSRelaunchAction") actionWithReason:@"RestartRenderServer" options:4 targetURL:[NSURL URLWithString:@"prefs:root=App%20Switcher%20Controller"]];
+		FBSSystemService *frontBoardService = [NSClassFromString(@"FBSSystemService") sharedService];
+		NSSet *actions = [NSSet setWithObject:respringAction];
+		[frontBoardService sendActions:actions withResult:nil];
 	}];
 	UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
 	[confirmRespringAlert addAction:cancel];
@@ -65,14 +81,19 @@ NSString *domainString = @"com.tomaszpoliszuk.appswitchercontroller";
 	[resetSettingsAlert addAction:confirm];
 	[self presentViewController:resetSettingsAlert animated:YES completion:nil];
 }
-
 -(void)sourceCode {
 	NSURL *sourceCode = [NSURL URLWithString:@"https://github.com/tomaszpoliszuk/AppSwitcherController"];
 	[[UIApplication sharedApplication] openURL:sourceCode options:@{} completionHandler:nil];
 }
--(void)reportIssueAtGithub {
-	NSURL *reportIssueAtGithub = [NSURL URLWithString:@"https://github.com/tomaszpoliszuk/AppSwitcherController/issues/new"];
-	[[UIApplication sharedApplication] openURL:reportIssueAtGithub options:@{} completionHandler:nil];
+-(void)knownIssues {
+	NSURL *knownIssues = [NSURL URLWithString:@"https://github.com/tomaszpoliszuk/AppSwitcherController/issues"];
+	[[UIApplication sharedApplication] openURL:knownIssues options:@{} completionHandler:nil];
+}
+-(void)TomaszPoliszukAtBigBoss {
+	UIApplication *application = [UIApplication sharedApplication];
+	NSString *tweakName = @"App+Switcher+Controller";
+	NSURL *twitterWebsite = [NSURL URLWithString:[@"http://apt.thebigboss.org/developer-packages.php?name=" stringByAppendingString:tweakName]];
+	[application openURL:twitterWebsite options:@{} completionHandler:nil];
 }
 -(void)TomaszPoliszukAtGithub {
 	UIApplication *application = [UIApplication sharedApplication];
